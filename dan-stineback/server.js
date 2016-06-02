18c1f2430cba5b9896e0ef52489a1af64cd109f3
Dan Stineback <dstineback@gmail.com>
2016-06-02 11:46:27 -0700

    took out test route

--- dan-stineback/server.js
+++ dan-stineback/server.js
@@ -10,14 +10,6 @@ mongoose.connect('mongodb://localhost/dev_db');
 
 app.use('/', authRouter);
 
-app.get('/test', (req, res) => {
-  res.send('does not need a token');
-});
-
-app.post('/test', bodyParser, jwtAuth, (req, res) => {
-  res.json({message:'need a token', user: req.user});
-});
-
 app.use((err, req, res, next) => {
   res.status(500).json({message: err.message});
   next(err);
